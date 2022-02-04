e2ab194466bcf3c6a58bbb008f6ad82344fc6c43
Mads Ehrhorn <ehrhorn@me.com>
2022-02-04 12:43:11 +0100

    Added route for single city fetching.

--- src/handler.ts
+++ src/handler.ts
@@ -1,11 +1,13 @@
 import { Router } from 'itty-router'
 
 import Cities from './handlers/cities'
+import City from './handlers/city'
 
 const router = Router()
 
 router
   .get('/api/cities', Cities)
+  .get('/api/cities/:id', City)
   .get('*', () => new Response('Not found', { status: 404 }))
 
 export const handleRequest = (request: any) => router.handle(request)
